995fd38542de2a8a0b90c8c5441bf4643cede0a0
Mykola Marenich <marenich.mykola@gmail.com>
2020-03-15 22:08:29 +0100

    script1

---
+++ handler/handlerSound.js
@@ -0,0 +1,9 @@
+function keyDown(e) {
+    debugger;
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+};
